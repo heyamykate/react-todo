c44a708fc5dfe3051b4a6e527ba832ef198e7b7a
Amy-Kate Andrews <amy-kate.andrews@wearetopsecret.com>
2015-05-18 12:15:34 -0600

    adds files

---
+++ js/components/TaskList.js
@@ -0,0 +1,19 @@
+var TaskList = React.createClass({
+    
+    
+    render: function() {
+        
+        var displayTask = function(task) {
+            return <li>{task}</li>
+        };
+        
+        return (
+            <ul>
+            { this.props.items.map(displayTask) }
+            </ul>        
+        );
+    }
+    
+});
+
+React.render(<TaskList />, document.body);
